f3a835c36f471ff7b15ce6e6287c2f0b47fb34d0
Niall Ferguson <nferguson@macmillan.org.uk>
2017-07-14 16:20:02 +0100

    fixed issues

--- ClientApp/app/app.module.client.ts
+++ ClientApp/app/app.module.client.ts
@@ -3,30 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { sharedConfig } from './app.module.shared';
-import { ModalModule } from 'angular2-modal';
-import { BootstrapModalModule } from 'angular2-modal/plugins/bootstrap';
-import { AppComponent } from './components/app/app.component';
-import { HeaderComponent } from './components/shared/header.component';
-import { FooterComponent } from './components/shared/footer.component';
-import { RouterModule } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { ContactComponent } from './components/contact/contact.component';
-
 
 @NgModule({
+    bootstrap: sharedConfig.bootstrap,
+    declarations: sharedConfig.declarations,
     imports: [
         BrowserModule,
         FormsModule,
         HttpModule,
-        ModalModule.forRoot(),
-        BootstrapModalModule
+        ...sharedConfig.imports
     ],
-    declarations: [
-        AppComponent, HomeComponent, ContactComponent, FooterComponent, HeaderComponent
-    ],
-    exports: [
-        RouterModule
-    ],
-    bootstrap: [AppComponent]
+    providers: [
+        { provide: 'ORIGIN_URL', useValue: location.origin }
+    ]
 })
-export class AppModule { }
+export class AppModule {
+}
